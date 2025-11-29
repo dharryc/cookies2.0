7f806d75a921b9a367b8a2743c634b46617e7a34
dharryc <dharrisoncook@gmail.com>
2025-11-29 01:55:13 -0700

    cors

--- backend/main.py
+++ backend/main.py
@@ -128,6 +128,8 @@ origins = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://cookiesgifts.com",
+    "https://api.cookiesgifts.com",
 ]
 
 app.add_middleware(
